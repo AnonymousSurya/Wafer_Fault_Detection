81dd6114f000512c7857855eafd0143799d9758a
AnonymousSurya <suryanarayanmohapatra99@gmail.com>
2023-08-22 07:10:58 +0530

    first commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+# AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "Waferfault_detection_project"
+MONGO_COLLECTION_NAME = "sensors_data"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://AnonymousSurya:mongoDB1234@surya.1vzdoh5.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
